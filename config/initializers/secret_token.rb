b5f29767c40af0e1f0157b3dfb02728660c813bb
Aaron Holmes <aholmes@pureguru-software.com>
2012-07-17 20:33:38 -0400

    Initial Rails Project Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BowlingStatsRails::Application.config.secret_token = 'ab17a24ff535983e47b7ece32f3c3017ba616abdcac39e48052eb0c5216b3dbdf9e071444591f3f94561c90868358fd0b9d4bc8691d3ec586f1112db53565f9c'
