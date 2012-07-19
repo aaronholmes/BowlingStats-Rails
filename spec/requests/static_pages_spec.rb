0e8bb2a47747151e8863e9e9da495c5527ae911c
Aaron Holmes <aholmes@pureguru-software.com>
2012-07-18 21:15:29 -0400

    Added more static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+describe "Static Pages" do
+  describe "Home Page" do
+    it "should have the content 'Welcome'" do
+      visit '/static_pages/home'
+      page.should have_content('Welcome')
+    end
+  end
+  
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
     end
   end
 end
