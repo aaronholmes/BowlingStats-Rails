3762eec0231b1ef2ebdd83014af3c0c51c06d50b
Aaron Holmes <aholmes@pureguru-software.com>
2012-07-24 22:01:26 -0400

    Updated users profile page and signup, added typekit, forced ssl to production

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+	# Returns the Gravatar (http://gravatar.com/) for the given user.
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+  def profile_image_for(user)
+  	#TODO: Replace with profile image code
+  	gravatar_id = Digest::MD5::hexdigest("example@railstutorial.org")
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
