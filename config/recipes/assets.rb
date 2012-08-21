c2b1637996534a6713d588ae20447f6a6536af1f
Aaron Holmes <aholmes@pureguru-software.com>
2012-08-21 18:51:01 -0400

    grrr

--- config/recipes/assets.rb
+++ config/recipes/assets.rb
@@ -1,7 +1,7 @@
 namespace :assets do
 	desc "Compile assets for production"
 	task :compile, :roles => :web do
-		run "cd #{release_path} && RAILS_ENV=#{rails_env} bundle exec rake cdn:upload:all --trace"
+		run "cd #{release_path} && RAILS_ENV=#{rails_env} bundle exec rake cdn:upload:all RAKE_USER=pete RAKE_API=232123 RAKE_FILES=booo --trace"
 	end
 end
 
