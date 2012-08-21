51a1612619a04e1f11d44d1ee5720f21790f254c
Aaron Holmes <aholmes@pureguru-software.com>
2012-08-21 17:54:27 -0400

    Added environment variables and changed api key

--- config/recipes/assets.rb
+++ config/recipes/assets.rb
@@ -1,7 +1,7 @@
 namespace :assets do
 	desc "Compile assets for production"
 	task :compile, :roles => :web do
-		run "cd #{release_path} && RAILS_ENV=#{rails_env} bundle exec rake cdn:upload:all --trace"
+		run "cd #{release_path} && RAILS_ENV=#{rails_env} RACK_USER=#{RACK_USER} bundle exec rake cdn:upload:all --trace"
 	end
 end
 
